83667e03a45962d64fe82ac0b3fcca39ccf7dd96
yfractal <mike@beansmile.com>
2014-06-20 01:44:13 +0800

    refactor code and add of which open the first function

--- open_pkg.jl
+++ open_pkg.jl
@@ -1,18 +1,38 @@
 # open a pkg
 PKG_DIR = Pkg.dir()
 
-macro op(pkg)
-    if haskey(ENV,"EDITOR")
-        editor = ENV["EDITOR"]
-    else
-        editor = "emacs"
-    end
-    
-    dir = joinpath(PKG_DIR,string(pkg))
-    comment_str = string(editor," ",dir)
-    
+if haskey(ENV,"EDITOR")
+    EDITOR = ENV["EDITOR"]
+else
+    EDITOR = "emacs"
+end
+
+function _open_method(method_target)
+    comment_str = string(EDITOR," ",method_target)
     e = Expr(:macrocall, symbol("@cmd"),comment_str)
-    
     run(eval(e))
 end
-# open a method
+
+macro op(pkg)
+    target = joinpath(PKG_DIR,string(pkg))
+
+    _open_method(target)
+end
+# @op JSON
+
+
+function get_method_target(m::Method)
+    li = m.func.code
+    string(li.file,":",li.line)
+end
+
+# open the first method
+function of(f::Function)
+    mt = methods(json)
+    m = mt.defs # get Method
+
+    method_target = get_method_target(m)
+    _open_method(m)
+end
+# using JSON
+# of(json)
